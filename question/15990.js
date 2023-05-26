e3ab269d85b1c352c73b63ba23501288ba934507
dingPie <cpie1216@gmail.com>
2023-05-26 13:59:54 +0900

    15990 답보고 구현, 빈곳찾기 로직 정리

--- question/15990.js
+++ question/15990.js
@@ -10,11 +10,23 @@ const numbers = input.slice(1).map((v) => Number(v));
 const max = Math.max(...numbers);
 
 const dp = {
-  1: 1,
-  2: 1,
-  3: 3,
+  1: [1, 0, 0], // 각 1, 2, 3을고 끝나는 수가 몇개인지. 이걸 reduce로 합치면 총 갯수가 된다.
+  2: [0, 1, 0],
+  3: [1, 1, 1], // 12 21
 };
 
+for (let i = 4; i <= max; i++) {
+  const one = (dp[i - 1][1] + dp[i - 1][2]) % 1000000009;
+  const two = (dp[i - 2][0] + dp[i - 2][2]) % 1000000009;
+  const three = (dp[i - 3][0] + dp[i - 3][1]) % 1000000009;
+
+  dp[i] = [one, two, three];
+}
+const answer = numbers.map(
+  (v) => dp[v].reduce((a, c) => a + c, 0) % 1000000009
+);
+
+console.log(answer.join("\n"));
 // 각 자릿수를 특정 숫자로 끝나는걸..따로 기록해서..
 // 결과에선 index를 더해서.
 
@@ -79,37 +91,37 @@ const dp = {
 // 3 1 3 1
 // 3 2 3
 
-const arr = [1, 2, 3];
-const TARGET = 7;
-let tt = 0;
-const test = (arr, num, picked = []) => {
-  arr.forEach((v, i) => {
-    if (picked.at(-1) !== v && num > v) test(arr, num - v, [...picked, v]);
-    else if (num === v && picked.at(-1) !== v) {
-      // console.log(num, v, [...picked, v]);
-      tt++;
-    }
-  });
-};
-test(arr, TARGET);
-console.log("tt", tt);
+// const arr = [1, 2, 3];
+// const TARGET = 7;
+// let tt = 0;
+// const test = (arr, num, picked = []) => {
+//   arr.forEach((v, i) => {
+//     if (picked.at(-1) !== v && num > v) test(arr, num - v, [...picked, v]);
+//     else if (num === v && picked.at(-1) !== v) {
+//       // console.log(num, v, [...picked, v]);
+//       tt++;
+//     }
+//   });
+// };
+// test(arr, TARGET);
+// console.log("tt", tt);
 
-const testArr = Array(16)
-  .fill(0)
-  .map((_, i) => i + 1);
+// const testArr = Array(16)
+//   .fill(0)
+//   .map((_, i) => i + 1);
 
-const ttt = testArr.map((v) => {
-  let value = 0;
-  const test = (arr, num, picked = []) => {
-    arr.forEach((v, i) => {
-      if (picked.at(-1) !== v && num > v) test(arr, num - v, [...picked, v]);
-      else if (num === v && picked.at(-1) !== v) {
-        // console.log(num, v, [...picked, v]);
-        value++;
-      }
-    });
-  };
-  test([1, 2, 3], v);
-  return { [v]: value };
-});
-console.log(ttt);
+// const ttt = testArr.map((v) => {
+//   let value = 0;
+//   const test = (arr, num, picked = []) => {
+//     arr.forEach((v, i) => {
+//       if (picked.at(-1) !== v && num > v) test(arr, num - v, [...picked, v]);
+//       else if (num === v && picked.at(-1) !== v) {
+//         // console.log(num, v, [...picked, v]);
+//         value++;
+//       }
+//     });
+//   };
+//   test([1, 2, 3], v);
+//   return { [v]: value };
+// });
+// console.log(ttt);
--- test.js
+++ test.js
@@ -1,63 +1,64 @@
-// 시작 좌표로 해서 한칸씩 가는건?
-// 작은글자? 13, 큰글자 30
-// 7px -> 1칸, 글자는 2칸
-// 작은글자 14px
-// 큰 글자 28px
-//
-
-const WIDTH = 280;
+const TEST_WORD = "여섯글자 단어";
+const WIDTH = 200;
 const HEIGHT = 160;
 
-// 모든 글자는 xxx xxx인 상태로 가정.
-// 그럼 총 13칸을 먹음.
+const BASE_FONT_PIXEL = 13;
+const BASE_FONT_PIXEL_HEIGHT = 2;
+
+// 총 단어 cell 길이(공백1, 한글2로 계산 )
+const wordCellLength = TEST_WORD.length * 2 - TEST_WORD.match(/\s/g).length;
+
+const wordLength = wordCellLength * (BASE_FONT_PIXEL / 2);
+const fontHeight = BASE_FONT_PIXEL_HEIGHT * (BASE_FONT_PIXEL / 2);
+
+// 모든 글자는 "여섯글자 단어" 인 상태로 가정.
+// 그럼 총 13칸을 먹음. (공백포함)
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-const setGrid = () => {
+const getWordCloud = () => {
   const board = Array(HEIGHT)
     .fill(0)
     .map(() => Array(WIDTH).fill(true));
 
-  const words = Array(10).fill("여섯글자 단어");
+  const words = Array(10).fill(TEST_WORD);
   const picked = [];
 
-  const WORD_LENGTH = 13 * 7;
-  const WORD_HEIGHT = 2 * 7;
-
   while (!!words.length) {
-    const wordWidth = picked.length < 3 ? 2 * WORD_LENGTH : WORD_LENGTH;
-    const wordHeight = picked.length < 3 ? WORD_HEIGHT * 2 : WORD_HEIGHT;
+    const wordWidth = picked.length < 3 ? 2 * wordLength : wordLength;
+    const wordHeight = picked.length < 3 ? fontHeight * 2 : fontHeight;
+
     const startX = getRandomInt(WIDTH - wordWidth);
     const startY = getRandomInt(HEIGHT - wordHeight);
+
     let posX = startX;
     let posY = startY;
 
     const size = { width: wordWidth, height: wordHeight };
 
     while (true) {
-      // posX + size.width < WIDTH && posY + size.height < HEIGHT
+      // 성공로직
       if (isSticky({ board, posX, posY, size })) {
-        // 성공로직
-        console.log("성공로직", startX, posX, startY, posY);
         isConvertFalse({ board, posX, posY, size });
         const target = words.shift();
         picked.push([target, posX, posY, size]);
         break;
       }
       //   실패로직
-      if (posX + size.width < WIDTH) {
+      if (posX + size.width < WIDTH - 1) {
         posX++;
-      } else if (posY + size.height < HEIGHT) {
+      } else if (posY + size.height < HEIGHT - 1) {
         posX = 0;
         posY += 1;
       } else if (posX + size.width === WIDTH && posY + size.height === HEIGHT) {
         posX = 0;
         posY = 0;
       } else {
-        console.log("전체단어 추가 불가");
+        console.log("전체단어 추가 불가.");
         return picked;
       }
     }
   }
+
   console.log("전체단어 추가 성공");
   return picked;
 };
@@ -80,20 +81,97 @@ const isConvertFalse = ({ board, posX, posY, size }) => {
   }
 };
 
-const test = setGrid();
+const wordCloud = getWordCloud();
 
-console.log("추가된 단어", test);
-
+// HTML에 추가
 const $box = document.querySelector(".box");
+$box.style.width = WIDTH + "px";
+$box.style.height = HEIGHT + "px";
 
-test.forEach(([target, posX, posY, size], i) => {
+wordCloud.forEach(([target, posX, posY, size], i) => {
   const _span = document.createElement("span");
   const text = document.createTextNode(target);
   _span.style.position = "absolute";
-  _span.style.fontSize = i < 3 ? "26px" : "13px";
+  _span.style.fontSize =
+    i < 3 ? BASE_FONT_PIXEL * 2 + "px" : BASE_FONT_PIXEL + "px";
   _span.style.left = posX + "px";
   _span.style.top = posY + "px";
 
   _span.appendChild(text);
   $box.append(_span);
 });
+
+// 이전 랜덤뽑기 시도로직
+
+// const getRandomInt = (max: number) => Math.floor(Math.random() * max);
+
+// const getRandom = ({
+//   words,
+//   size,
+// }: {
+//   words: string[];
+//   size: { width: number; height: number };
+// }) => {
+//   const picked: {
+//     word: string;
+//     position: { x: number; y: number };
+//   }[] = [];
+//   const rest = [...words];
+
+//   while (rest.length) {
+//     const targetWord = rest[0];
+//     const 공백길이 = targetWord.match(/\s/);
+
+//     const BIG_FONT_WIDTH = 25;
+//     const SMALL_FONT_WIDTH = 13;
+//     const BIG_FONT_HEIGHT = 52;
+//     const SMALL_FONT_HEIGHT = 27;
+
+//     const fontWidth =
+//       picked.length < 3
+//         ? targetWord.length * BIG_FONT_WIDTH
+//         : targetWord.length * SMALL_FONT_WIDTH;
+//     const fontHeight = picked.length < 3 ? BIG_FONT_HEIGHT : SMALL_FONT_HEIGHT;
+
+//     const randomX = getRandomInt(size.width - fontWidth);
+//     const randomY = getRandomInt(size.height - fontHeight);
+
+//     const newSx = randomX;
+//     const newEx = randomX + fontWidth;
+//     const newSy = randomY;
+//     const newEy = randomY + fontWidth;
+
+//     const isCanAdd = picked?.some((v, i) => {
+//       const targetFontWidth =
+//         i < 3
+//           ? v.word.length * BIG_FONT_WIDTH
+//           : v.word.length * SMALL_FONT_WIDTH;
+//       const targetFontHeight = i < 3 ? BIG_FONT_HEIGHT : SMALL_FONT_HEIGHT;
+
+//       // if (i < 3) {
+//       //   // big
+//       const sx = v.position.x;
+//       const ex = v.position.x + targetFontWidth;
+//       const sy = v.position.y;
+//       const ey = v.position.y + targetFontHeight;
+
+//       return newEx >= sx && newSx <= ex && newEy >= sy && newSy <= ey;
+//     });
+//     if (isCanAdd) continue;
+
+//     rest.shift();
+//     targetWord &&
+//       picked.push({ word: targetWord, position: { x: randomX, y: randomY } });
+//   }
+
+//   return picked;
+// };
+
+// const BIG_WORD = {
+//   x: 120,
+//   y: 52,
+// };
+// const SMALL_WORD = {
+//   x: 51,
+//   y: 27,
+// };
