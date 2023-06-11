e839e943466c643e43a6aa09fcc11c486927148b
dingPie <cpie1216@gmail.com>
2023-06-12 07:58:17 +0900

    11867푸는ㄴ중

--- question/17291.js
+++ question/17291.js
@@ -13,25 +13,23 @@ const dp = {
   1: 1,
 };
 
+const birthObj = {
+  0: 0,
+  1: 1,
+};
+
 for (let i = 2; i <= n; i++) {
   let newBug = dp[i - 1] * 2;
-  if (i - 3 > 0 && (i - 3) % 2 === 1) {
-    const created = dp[i - 3] - dp[i - 4];
-    newBug -= created;
-    // console.log("홀수에 태어난 애들", created);
+  birthObj[i] = dp[i - 1];
+
+  if (i > 3 && (i - 3) % 2 === 1) {
+    newBug -= birthObj[i - 3];
   }
-  if (i - 4 > 0 && (i - 4) % 2 === 0) {
-    const created = dp[i - 4] - dp[i - 5];
-    // console.log("짝수에 태어난 애들", created);
-    newBug -= created;
+  if (i > 4 && (i - 4) % 2 === 0) {
+    newBug -= birthObj[i - 4];
   }
 
   dp[i] = newBug;
 }
 
-console.log(dp);
-
-// 일단...dp로 기록해서 n년차에 몇마리가 태어났는지 계산할 수 있다.
-// 사망은 분열하고 -> 사망이라 수가 줄어들진 않는다.
-
-// 맞는것같은데... 이전 구한 값에서 태어난 마릿수를 계산해주는 로직..그만큼 계산해서 죽이고..
+console.log(dp[n]);
