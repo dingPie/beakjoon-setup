f9722b7398dc0e945859454a2cd884303d08b8f6
dingPie <cpie1216@gmail.com>
2023-07-11 07:59:37 +0900

    15992 푸는중;

---
+++ question/15992.js
@@ -0,0 +1,58 @@
+// // 3. 여러 줄의 값들을 입력받을 때
+const fileName = __filename.split("/question/")[1].split(".js")[0];
+const dir = `../test/${fileName}.txt`;
+// const dir = "/dev/stdin";
+
+const fs = require("fs");
+const input = fs.readFileSync(dir).toString().trim().split("\n");
+
+const n = Number(input[0]);
+
+let max = 0;
+
+const nums = input.slice(1).map((v) =>
+  v.split(" ").map((v) => {
+    if (Number(v) > max) max = Number(v);
+    return Number(v);
+  })
+);
+
+const dp = Array(max)
+  .fill(0)
+  .map((v) => Array(max).fill(0));
+
+for (let i = 1; i < max; i++) {
+  for (let j = 1; j < max; j++) {
+    dp[i][j] = dp[i][j];
+  }
+}
+
+console.log(dp);
+
+// 최대값인 1000 * 1000 돌려봐야 ㄱㅊ은듯.
+
+// const dp = {
+//   1: [0, 1, 0, 0, 0],
+//   2: [0, 1, 1, 0, 0], // 11, 2
+//   3: [0, 1, 2, 1, 0], // 111 12 21 3
+//   4: [0, 0, 3, 3, 1], // 111 12 21 3
+//   5: [0, 0, 2, 6, 4, 1], // 111 12 21 3
+// };
+
+//  i-3 i-2 i-1 의 자릿수를 더해서 하나씩 밀면된다.
+
+// 111 12 21 3
+// 11 2
+// 1 2 4
+// const
+
+// 1 2 4 7 13
+
+// 11111
+// 1112 1121 1211 2111
+// 113 131 311 122 212 221
+// 23 32
+//
+
+// 만약 9의 경우 3자리 (333) 부터 9자리.
+// Math.floor(m / 3) 이 최소인듯.
--- question/2780.js
+++ question/2780.js
@@ -12,37 +12,32 @@ const mod = 1234567;
 
 const max = Math.max(...numbers);
 
-// const near = [2, 3, 2, 3, 4, 3, 3, 3, 2, 1];
+const near = [2, 3, 2, 3, 4, 3, 3, 3, 2, 1];
 
-// const dp = {
-//   1: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-// };
+const dp = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+const canNums = [
+  [7],
+  [2, 4],
+  [1, 3, 5],
+  [2, 6],
+  [1, 5, 7],
+  [2, 4, 6, 8],
+  [3, 5, 9],
+  [4, 8, 0],
+  [5, 7, 9],
+  [6, 8],
+];
 
-// const near = [2n, 3n, 2n, 3n, 4n, 3n, 3n, 3n, 2n, 1n];
-
-// const dp = {
-//   1: [1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n],
-// };
-
-for (let i = 2; i <= max; i++) {
-  dp[i] = dp[i - 1].map((v, i) => (v * near[i]) % mod);
+for (let i = 1; i <= max; i++) {
+  const t = Array(10).fill(0);
+  for (let j = 0; j <= 9; j++) {
+    t[j] = canNums[j].reduce((a, c) => a + dp[i - 1][c], 0) % mod;
+  }
+  dp[i] = t;
 }
 
-const answer = numbers.map((v) => dp[v].reduce((a, c) => a + c, 0) % mod);
+const answer = numbers.map((v) => dp[v - 1].reduce((a, c) => a + c, 0) % mod);
 
 console.log(answer.join("\n"));
 
 // 왜안되지..
-
-const canNums = {
-  1: [2, 4],
-  2: [1, 3, 5],
-  3: [2, 6],
-  4: [1, 5, 7],
-  5: [2, 4, 6, 8],
-  6: [3, 5, 9],
-  7: [4, 8, 0],
-  8: [5, 7, 9],
-  9: [6, 8],
-  0: [7],
-};
