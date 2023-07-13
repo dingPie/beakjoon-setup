7f610cc748642c7571a0fac0d27ea41fc8faa171
dingPie <cpie1216@gmail.com>
2023-07-13 14:01:17 +0900

    왜안되지

--- pro.js
+++ pro.js
@@ -8,17 +8,51 @@
 // 냅색 알고리즘이랑 비슷하니까 2차원 배열로 구성해야 될 것 같고
 // 어떻게 채워나가지 ?
 
-const sticker = [14, 6, 5, 11, 3, 9, 2, 10];
-const n = sticker.length;
+const sticker = [14, 6];
+function solution(sticker) {
+  const n = sticker.length;
+  if (n === 1) return sticker[0];
+  else if (n === 2) return Math.max(sticker[0], sticker[1]);
+  const dp = Array(n).fill(0);
 
-const dp = Array(n)
-  .fill(0)
-  .map((v) => Array(n).fill(0));
+  if (sticker[0] - sticker.at(-1) > 0) {
+    sticker[0] -= sticker.at(-1);
+  }
 
-console.log(dp);
+  dp[0] = sticker[0];
+  dp[1] = Math.max(sticker[0], sticker[1]);
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + sticker[i]);
+  }
+
+  return dp.at(-1);
+}
+const answer = solution(sticker);
+console.log(answer);
+
+// 효율성은 싹 통과 d
 
 // n번째 숫자까지만? 전부다? dp돌려?
 
 // 계단처럼 n번쨰를 진행했을 떄 잃게되는 수를 기록해야 할 듯?
 
 //
+
+// 이걸 고르고 이게 빠진 dp를 고르느냐 /
+// 이걸 안고르고 그냥 이전 dp를 이어가느냐 중 어떤게 더 큰지?
+
+// 골랐을 떄 어떻게 처리하지?
+
+// 3개중 어떤걸 선택?
+
+//  [14, 6, 5, 11, 3, 9, 2, 10];
+
+// 14-10
+// 4 6
+// 4 6 9
+// 4 6 9 17
+// 4 6 9 17 17
+// 4 6 9 17 17 26
+// 4 6 9 17 17 26 26
+// 4 6 9 17 17 26 26 36
