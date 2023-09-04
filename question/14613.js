de378effdf4a9a7bd248d28ea80a5c4078251523
dingPie <cpie1216@gmail.com>
2023-09-04 14:01:12 +0900

    14613 거의다품

--- question/14613.js
+++ question/14613.js
@@ -13,39 +13,64 @@ const [w, l, d] = input.map((v) => Number(v));
 // 플래티넘: 2500~2999
 // 다이아: 3000~3499
 // 소수점 8자리까지 출력
-console.log(w, l, d);
 
 let count = 20;
 let score = 2_000; // 시작점수
 
 // 게임 수는 20 판, 각 판을 진행할 떄 마다 확률을 각각 계산?
 
-const tt = (num) => Math.round(num * 10_000_000) / 10_000_000;
-
-const dp = {
+let dp = {
   2000: 1,
 };
-// 현재의 확률을 다 나눠주고
-// 결과의 확률만 더해준다?
-//
+
 for (let i = 0; i < count; i++) {
+  const newDp = { ...dp };
+
   for (const s in dp) {
+    if (dp[s] === 0) continue;
     const wScore = Number(s) + 50;
     const lScore = Number(s) - 50;
-    if (!dp[wScore]) dp[wScore] = 0;
-    if (!dp[lScore]) dp[lScore] = 0;
-    dp[wScore] = tt(w * dp[s]);
-    dp[lScore] = tt(l * dp[s]);
-    dp[s] = tt(d * dp[s]);
+    if (!newDp[wScore]) newDp[wScore] = 0;
+    if (!newDp[lScore]) newDp[lScore] = 0;
+    newDp[wScore] = newDp[wScore] + w * dp[s];
+    newDp[lScore] = newDp[lScore] + l * dp[s];
+    newDp[s] = d * dp[s];
   }
-  console.log(dp);
+
+  dp = newDp;
 }
 
-console.log(dp);
+// 이거 가변확률 적용되서 그런거같은데
+// newDp에 더하는 식으로 해보자 다시....
 
-// 이거 -7 자리 이런식으로 뜸.
-// 소숫점도 bigInt가 가능한가?
+// 아니 풀었는데
+// 소숫점 몇째자리 이게 개에바네
 
-// ㅅㅂ d여기서 확률을 어케넣어
+// console.log(dp);
+const answer = {
+  bronze: 0,
+  silver: 0,
+  gold: 0,
+  platinum: 0,
+  diamond: 0,
+};
 
-// 현재 값에서 승 /패 확률을 더한값.
+for (const s in dp) {
+  if (Number(s) < 1500) {
+    answer.bronze += dp[s];
+  } else if (Number(s) >= 1500 && Number(s) < 2000) {
+    answer.silver += dp[s];
+  } else if (Number(s) >= 2000 && Number(s) < 2500) {
+    answer.gold += dp[s];
+  } else if (Number(s) >= 2500 && Number(s) < 3000) {
+    answer.platinum += dp[s];
+  } else {
+    answer.diamond += dp[s];
+  }
+}
+
+for (const k in answer) {
+  console.log(answer[k].toFixed(8));
+}
+
+// 한 10 % 쯤에서 틀림. 거의다풀었따 .
